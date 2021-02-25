45df66ad6d595e200e1d261ddf21413bea4bf5ec
Miguel Gonçalves <73142905+Miguelii@users.noreply.github.com>
2021-02-25 13:23:40 +0000

    Update triangulo.c

--- Ficha1/triangulo.c
+++ Ficha1/triangulo.c
@@ -1,11 +1,5 @@
 
-/* Para correr no terminal:
-    gcc -c triangulo.c
-    gcc -c vertical.c
-    gcc -c horizontal.c
-    gcc -o triv triangulo.o vertical.o
-    gcc -o trih triangulo.o horizontal.o
-*/
+
 
 void triangulo (int n);
 
