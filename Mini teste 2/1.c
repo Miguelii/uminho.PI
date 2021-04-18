bee61f0f3bbdaf6a7412c972433dab40da971296
Miguel Gonçalves <73142905+Miguelii@users.noreply.github.com>
2021-04-18 23:09:42 +0100

    Update 1.c

--- Mini teste 2/1.c
+++ Mini teste 2/1.c
@@ -1,4 +1,24 @@
 #include <stdio.h>
+/* Enunciado Pergunta 1
+
+int sumhtpo (int n){
+  int r = 0;
+  while (n != 1) {
+    r += n;
+    if (n%2 == 0) n = n/2;
+    else n = 1+(3*n);
+  }
+  return r;
+}
+
+This function computes a sum. For instance, if n == 6, the computed sum has 8 items:
+
+6 + 3 + 10 + 5 + 16 + 8 + 4 + 2 = 55
+
+For n = 27 how many items has the computed sum?
+*/
+
+// resolução:
 
 int main() {
     int r=0;
