01b6166a7e3e597dd843ccd34158f899cf8c389d
Miguel Gonçalves <73142905+Miguelii@users.noreply.github.com>
2021-05-05 00:13:29 +0100

    Update 1.c

--- Mini testes/Mini4/1.c
+++ Mini testes/Mini4/1.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
+/*  Relembre a seguinte função.
 
+int sumhtpo (int n){
+  int r = 0;
+  while (n != 1) {
+    r += n;
+    if (n%2 == 0) n = n/2;
+    else n = 1+(3*n);
+  }
+  return r;
+}
+
+Esta função calcula um somatório. Para n = 97 a menor parcela desse somatório é 2. A 2ª menor é 4. Qual é a 30ª menor parcela?
+
+*/
+
+// Resolução
 
 int main() {
     int r=0;
@@ -8,6 +24,7 @@ int main() {
 
     while (n!=1) {
         r += n;
+        printf("%d \n", n);
         
         if (n%2 == 0) {
             n = n/2;
@@ -16,7 +33,6 @@ int main() {
         else {
             n = 1+(3*n);
         }
-        printf("%d \n", n);
         c++;
         
     }
