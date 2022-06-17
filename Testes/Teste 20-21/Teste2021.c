74e2366dcda8d9f923f564c69cd7034c1b55747d
Miguel Gonçalves <miguelgoncalves18@hotmail.com>
2022-06-17 02:31:49 +0100

    Update Teste2021.c

--- Testes/Teste 20-21/Teste2021.c
+++ Testes/Teste 20-21/Teste2021.c
@@ -1,109 +1,161 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-// 1)
+typedef struct nodo {
+	int valor;
+	struct nodo *pai, *esq, *dir;
+} *ABin;
 
-void insereOrdenado (int array[],int number, int N){
-    int inserted = 0;
-    for (int i = 0; i< N;i++){
-        if (number < array[i]){
-            for (int u = N; u > i;u--)
-                array[u] = array[u-1];
-            array[i] = number;
-        }
-        if (number == array[i]) return;
-    }
-    if (!inserted) array[N] = number;
-}
+typedef struct lligada {
+	int valor;
+	struct lligada *prox;
+} *LInt, Celula;
 
-int sumhtpo (int n){
-    int array[100];
-    int count = 0;
-    while (n != 1) {
-        insereOrdenado (array,n,count++);
-        if (n%2 == 0) n = n/2;
-        else n = 1 + (3*n);
-        if (count > 100) count = 100;
-    }
-    if (count < 100) return -1;
-    return array[99];    
-}
-
-// 2)
-int moda (int v[], int N, int *m){
-    if (!N)return 0;
-    int repete = 0, numModa = 0, freq = 0;
-    for (int i = 0; i < N;i++){
-        int count = 1;
-        for (int u = i +1; u < N;u++)
-            if (v[u] == v[i]) count++;
-        if (count > numModa){
-            repete = 0;
-            numModa = count;
-            freq = v[i];
-        }
-        else if (count == numModa) {
-            repete = 1;
-        }
-    }
-    if (repete) return 0;
-    *m = numModa;
-    return freq;
-}
-
-// 3)
-typedef struct lint
-{
-    int valor;
-    struct lint* prox;
-}*LInt;
-
-int procura (LInt *l, int x) {
-    LInt *inicio = l;
-    int r = 0;
-    while(*inicio != NULL) {
-        if((*inicio)->valor == x) {
-            LInt temp = *inicio;
-            *inicio = (*inicio)->prox;
-            temp->prox = *l;
-            *l = temp;
-            r = 1;
-        }
-        inicio = &((*inicio)->prox);
+LInt newLInt (int x, LInt xs){
+    LInt r = malloc(sizeof(Celula));
+    if (r!=NULL) {
+        r->valor = x; r->prox = xs;
     }
     return r;
 }
+// -----------------------------------
 
-// 4)
+// PERGUNTA 1
 
-typedef struct nodo {
-    int valor;
-    struct nodo *pai, *esq, *dir;
-} *ABin;
+void printArry(int arr[],int N) {
 
-
-int freeAB (ABin a){
-    if (!a) return 0;
-    int dir = freeAB (a->dir);
-    int esq = freeAB (a->esq);
-    free (a);
-    return 1 + dir + esq;
+  int loop;
+  for(loop = 0; loop < N; loop++)
+    printf("%d ", arr[loop]);
 }
 
-// 5)
-
-void caminho (ABin a){
-    int array[100], N = 0;
-    while (a->pai)
-    {
-        ABin ant = a;
-        a = a->pai;
-        if (ant == a->dir) array[N++] = 0; 
-        else array[N++] = 1;
+void ordenaArray(int arr[], int N) {
+  int i;
+  int j;
+  for(int i=0;i<N;i++) {
+    for(int j=i+1;j<N;j++) {
+      if(arr[i]>arr[j]) {
+        int aux = arr[i];
+        arr[i] = arr[j];
+        arr[j] = aux;
+        }
     }
-    for (int i = 0; i < N;i++){
-        if (array[i] == 0)printf ("dir\n"); 
-        else printf ("esq\n");
+  }
+
+}
+
+int sumhtpo (int n) {
+  int arr[1024];
+  int index = 0;
+  int count = 0;
+
+  int r = 0;
+  while(n!=1) {
+    arr[index] = n;
+    index++;
+    //r += n;
+    if (n%2  == 0) n = n/2; else n = 1+(3*n);
+  }
+  
+  
+  ordenaArray(arr,index+1);
+  
+  printArry(arr,index+1);
+
+  return arr[99];
+}
+
+//PERGUNTA 2
+//Enunciado confuso, mas acho que era isto que pediam
+
+int moda(int v[], int N, int *m) {
+  int i;
+  int j;
+  int count = 0;
+  int maisfreq = 0;
+  
+  for(i=0;i<N;i++) {
+    for(j=i;j<N;j++) {
+      if(v[j]==v[i]) count++;
     }
     
+    if(count==maisfreq) return 0;
+    else if(count > maisfreq) {
+      maisfreq = count;
+      *m = v[i];
+    }
+    
+    count = 0;
+  }
+  
+  return maisfreq;
+}
+
+
+//PERGUNTA 3
+
+int procura (LInt *l, int x) {
+  
+  LInt *aux = l;
+  LInt temp = NULL;
+  
+  //Caso o x seja o elemento à cabeça
+  if((*aux)->valor == x) return 1;
+
+  while((*aux) != NULL && (*aux)->valor != x) {
+    aux = &((*aux)->prox);
+  }
+  
+  if((*aux) == NULL) return 0;
+  else {
+    temp = *aux;
+    *aux = (*aux)->prox;
+    free(temp);
+    
+    LInt novo = malloc (sizeof(Celula));
+    novo->valor = x;
+    novo->prox = *l;
+    *l = novo;
+    
+    return 1;
+  }
+}
+
+//PERGUNTA 4
+int freeAB (ABin a) {
+    
+  if(a==NULL) return 0;
+  
+  int esq = freeAB(a->esq);
+  int dir = freeAB(a->dir);
+  
+  free(a);
+  return 1+esq+dir;
+}
+
+int main() {
+
+  //Pergunta 1
+  int res = sumhtpo(1000);
+  printf("\n RES: %d",res);
+
+  //Pergunta 2
+  int v[] = {1,2,2,3,3,3,4,4,4,4,7,7,7,7,7};
+  int coiso;
+  int *m = &coiso;
+  
+  int res2 = moda(v,15,m);
+
+  //Pergunta 3
+  LInt l = NULL;
+  
+  l = newLInt(3,l);
+  l = newLInt(2,l);
+  l = newLInt(1,l);
+
+  procura(&l,2);
+
+
+  return 0;
 }
